f57f06fa67c51d07d9e8a19884c2898fe05f1fb3
KezhiAdore <1538001851@qq.com>
2022-10-07 15:32:09 +0800

    :recycle: refactor: use auto pep8

--- pattern_generator.py
+++ pattern_generator.py
@@ -7,223 +7,239 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from multiprocessing import Pool
 
-from settings import dataA_paths,dataB_paths,segments_path,segment_figures_dir
-from utils import Strip,Segment,dill_load,dill_save
+from settings import dataA_paths, dataB_paths, segments_path, segment_figures_dir
+from utils import Strip, Segment, dill_load, dill_save
 import settings
 
-for name,value in settings.__dict__.items():
+for name, value in settings.__dict__.items():
     if "dir" in name:
-        dir_path=value
+        dir_path = value
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
 
+
 class PatternGenerator:
-    
-    def __init__(self,df:pd.DataFrame,L,W) -> None:
-        self._origin_df=df
-        self._item_shape=[]
-        self._item_require_num={}
-        self._item_product_num={}
+
+    def __init__(self, df: pd.DataFrame, L, W) -> None:
+        self._origin_df = df
+        self._item_shape = []
+        self._item_require_num = {}
+        self._item_product_num = {}
         self.init_item()
-        
-        self._L=L   # 原料版长度
-        self._W=W   # 原料板宽度
-        self._interval=1
-        
-        self._q=self.q_set()
-        self._strips=self.generate_strips()
-        
-        self._segments=[]
+
+        self._L = L   # 原料版长度
+        self._W = W   # 原料板宽度
+        self._interval = 1
+
+        self._q = self.q_set()
+        self._strips = self.generate_strips()
+
+        self._segments = []
         self.repeat_generate_segments()
-        
-        
+
     def init_item(self):
-        for _,row in self._origin_df.iterrows():
-            item_length=row["item_length"]
-            item_width=row["item_width"]
-            
-            self._item_shape.append((item_length,item_width))
-            self._item_shape.append((item_width,item_length))
-            
+        for _, row in self._origin_df.iterrows():
+            item_length = row["item_length"]
+            item_width = row["item_width"]
+
+            self._item_shape.append((item_length, item_width))
+            self._item_shape.append((item_width, item_length))
+
             if item_length not in self._item_require_num.keys():
-                self._item_require_num[item_length]={}
+                self._item_require_num[item_length] = {}
             if item_width not in self._item_require_num[item_length].keys():
-                self._item_require_num[item_length][item_width]=1
+                self._item_require_num[item_length][item_width] = 1
             else:
-                self._item_require_num[item_length][item_width]+=1
-                
+                self._item_require_num[item_length][item_width] += 1
+
         # item去重
-        self._item_shape=list(set(self._item_shape))
-        
-    def item_require_num(self,length,width):
+        self._item_shape = list(set(self._item_shape))
+
+    def item_require_num(self, length, width):
         if length in self._item_require_num.keys():
             if width in self._item_require_num[length].keys():
                 return self._item_require_num[length][width]
-        
+
         if width in self._item_require_num.keys():
             if length in self._item_require_num[width].keys():
-                    return self._item_require_num[width][length]
-        raise ValueError("can not find length={},width={} in item list".format(length,width))
-    
-    def reduce_item_require(self,length,width,reduce_num=1):
+                return self._item_require_num[width][length]
+        raise ValueError(
+            "can not find length={},width={} in item list".format(length, width))
+
+    def reduce_item_require(self, length, width, reduce_num=1):
         if length in self._item_require_num.keys():
             if width in self._item_require_num[length].keys():
-                if self._item_require_num[length][width]>=0:
-                    self._item_require_num[length][width]=max(self._item_require_num[length][width]-reduce_num,0)
+                if self._item_require_num[length][width] >= 0:
+                    self._item_require_num[length][width] = max(
+                        self._item_require_num[length][width]-reduce_num, 0)
                     return
                 else:
-                    raise ValueError(self._item_require_num[length][width],"should larger than 0")
-        
+                    raise ValueError(
+                        self._item_require_num[length][width], "should larger than 0")
+
         if width in self._item_require_num.keys():
             if length in self._item_require_num[width].keys():
-                if self._item_require_num[width][length]>=0:
-                    self._item_require_num[width][length]=max(self._item_require_num[width][length]-reduce_num,0)
+                if self._item_require_num[width][length] >= 0:
+                    self._item_require_num[width][length] = max(
+                        self._item_require_num[width][length]-reduce_num, 0)
                     return
                 else:
-                    raise ValueError(self._item_require_num[width][length],"should larger than 0")
-        
-        raise ValueError("can not find length={},width={} in item list".format(length,width))
-    
-    def item_product_num(self,length,width):
+                    raise ValueError(
+                        self._item_require_num[width][length], "should larger than 0")
+
+        raise ValueError(
+            "can not find length={},width={} in item list".format(length, width))
+
+    def item_product_num(self, length, width):
         if length in self._item_require_num.keys() and width in self._item_require_num[length].keys():
             if length not in self._item_product_num.keys():
-                self._item_product_num[length]={}
+                self._item_product_num[length] = {}
                 if width not in self._item_product_num[length].keys():
-                    self._item_product_num[length][width]=0
+                    self._item_product_num[length][width] = 0
             return self._item_product_num[length][width]
-        
+
         if width in self._item_require_num.keys() and length in self._item_require_num[width].keys():
             if width not in self._item_product_num.keys():
-                self._item_product_num[width]={}
+                self._item_product_num[width] = {}
                 if length not in self._item_product_num[width].keys():
-                    self._item_product_num[width][length]=0
+                    self._item_product_num[width][length] = 0
             return self._item_product_num[width][length]
-        raise ValueError("can not find length={},width={} in item list".format(length,width))
-    
+        raise ValueError(
+            "can not find length={},width={} in item list".format(length, width))
+
     def q_set(self):
-        q=[self._L]
+        q = [self._L]
         for shape in self._item_shape:
-            item_length=shape[0]
-            item_width=shape[1]
-            for k in range(min(self.item_require_num(item_length,item_width),int(self._L/item_length))+1):
+            item_length = shape[0]
+            item_width = shape[1]
+            for k in range(min(self.item_require_num(item_length, item_width), int(self._L/item_length))+1):
                 q.append(k*item_length)
-        q=list(set(q))
+        q = list(set(q))
         q.sort()
         return q
-    
+
     def generate_strips(self):
-        strips={}
+        strips = {}
         for x in self._q:
-            strips[x]=[]
+            strips[x] = []
             for shape in self._item_shape:
-                item_length=shape[0]
-                item_width=shape[1]
-                if item_length>x:
+                item_length = shape[0]
+                item_width = shape[1]
+                if item_length > x:
                     continue
-                item_value=item_length*item_width
-                e_max=min(int(x/item_length),self.item_require_num(item_length,item_width))
-                strips[x].append(Strip(x,item_length,item_width,e_max,item_value))
+                item_value = item_length*item_width
+                e_max = min(int(x/item_length),
+                            self.item_require_num(item_length, item_width))
+                strips[x].append(
+                    Strip(x, item_length, item_width, e_max, item_value))
         return strips
-    
+
     def repeat_generate_segments(self):
         if os.path.exists(segments_path):
-            self._segments=dill_load(segments_path)
+            self._segments = dill_load(segments_path)
         while not self.check_segments_coverage():
             self.reduce_require_item_from_segments()
             self._segments.extend(self.generate_segments())
-        dill_save(self._segments,segments_path)
+        dill_save(self._segments, segments_path)
         return self._segments
-               
+
     def generate_segments(self):
-        
-        segments=[]
+
+        segments = []
         with Pool(processes=8) as pool:
-            results=pool.map(self.generate_segment_x,[x for x in self._strips])
-        
-        for i,r in enumerate(results):
+            results = pool.map(self.generate_segment_x, [
+                               x for x in self._strips])
+
+        for i, r in enumerate(results):
             if not r.empty:
                 segments.append(r)
-        
+
         return segments
-    
-    def generate_segment_x(self,x):
+
+    def generate_segment_x(self, x):
         print("generate segment x: {}".format(x))
-        strips=self._strips[x]
+        strips = self._strips[x]
         # initial dp function
-        dp_F={}
-        dp_n={}
-        dp_segment={}
-        dp_F[0]=0
-        dp_segment[0]=Segment(x,[])
+        dp_F = {}
+        dp_n = {}
+        dp_segment = {}
+        dp_F[0] = 0
+        dp_segment[0] = Segment(x, [])
         for length in self._item_require_num.keys():
-            dp_n[length]={}
+            dp_n[length] = {}
             for width in self._item_require_num[length].keys():
-                dp_n[length][width]={}
-                dp_n[length][width][0]=0
-        
-        for y in np.arange(self._interval,self._W+self._interval,self._interval):
-            max_F=0
-            max_strip=None
-            max_e=0
+                dp_n[length][width] = {}
+                dp_n[length][width][0] = 0
+
+        for y in np.arange(self._interval, self._W+self._interval, self._interval):
+            max_F = 0
+            max_strip = None
+            max_e = 0
             for strip in strips:
-                item_width=strip.w
-                if math.ceil(item_width)>y:
+                item_width = strip.w
+                if math.ceil(item_width) > y:
                     continue
-                item_length=strip.l
+                item_length = strip.l
                 if item_length in self._item_require_num.keys() and item_width in self._item_require_num[item_length].keys():
-                    n=dp_n[item_length][item_width][y-math.ceil(item_width)]
+                    n = dp_n[item_length][item_width][y-math.ceil(item_width)]
                 elif item_width in self._item_require_num.keys() and item_length in self._item_require_num[item_width].keys():
-                    n=dp_n[item_width][item_length][y-math.ceil(item_width)]
+                    n = dp_n[item_width][item_length][y-math.ceil(item_width)]
                 else:
-                    raise ValueError("can not find length={},width={} in item list".format(length,width))
-                e_max=min(int(x/item_length),strip.e-n)
-                value=strip.v
-                new_F=dp_F[y-math.ceil(item_width)]+e_max*value
-                if new_F>max_F:
-                    max_F=copy.deepcopy(new_F)
-                    max_strip=copy.deepcopy(strip)
-                    max_e=copy.deepcopy(e_max)
-                    
-            if dp_F[y-self._interval]>=max_F:
-                dp_F[y]=dp_F[y-self._interval]
-                dp_segment[y]=copy.deepcopy(dp_segment[y-self._interval])
+                    raise ValueError(
+                        "can not find length={},width={} in item list".format(length, width))
+                e_max = min(int(x/item_length), strip.e-n)
+                value = strip.v
+                new_F = dp_F[y-math.ceil(item_width)]+e_max*value
+                if new_F > max_F:
+                    max_F = copy.deepcopy(new_F)
+                    max_strip = copy.deepcopy(strip)
+                    max_e = copy.deepcopy(e_max)
+
+            if dp_F[y-self._interval] >= max_F:
+                dp_F[y] = dp_F[y-self._interval]
+                dp_segment[y] = copy.deepcopy(dp_segment[y-self._interval])
                 for length in self._item_require_num.keys():
                     for width in self._item_require_num[length].keys():
-                        dp_n[length][width][y]=dp_n[length][width][y-self._interval]
+                        dp_n[length][width][y] = dp_n[length][width][y-self._interval]
             else:
-                item_width=max_strip.w
-                item_length=max_strip.l
-                dp_F[y]=max_F
-                dp_segment[y]=copy.deepcopy(dp_segment[y-math.ceil(item_width)])
-                dp_segment[y].append(Strip(x,item_length,item_width,max_e,max_strip.v))
+                item_width = max_strip.w
+                item_length = max_strip.l
+                dp_F[y] = max_F
+                dp_segment[y] = copy.deepcopy(
+                    dp_segment[y-math.ceil(item_width)])
+                dp_segment[y].append(
+                    Strip(x, item_length, item_width, max_e, max_strip.v))
                 for length in self._item_require_num.keys():
                     for width in self._item_require_num[length].keys():
-                        dp_n[length][width][y]=dp_n[length][width][y-math.ceil(item_width)]
-                if item_length==item_width:
-                    dp_n[item_length][item_width][y]+=max_e
+                        dp_n[length][width][y] = dp_n[length][width][y -
+                                                                     math.ceil(item_width)]
+                if item_length == item_width:
+                    dp_n[item_length][item_width][y] += max_e
                 elif item_length in self._item_require_num.keys() and item_width in self._item_require_num[item_length].keys():
-                    dp_n[item_length][item_width][y]+=max_e
+                    dp_n[item_length][item_width][y] += max_e
                 elif item_width in self._item_require_num.keys() and item_length in self._item_require_num[item_width].keys():
-                    dp_n[item_width][item_length][y]+=max_e
+                    dp_n[item_width][item_length][y] += max_e
                 else:
-                    raise ValueError("can not find length={},width={} in item list".format(length,width))
-        
-        best_segment=dp_segment[self._W]
+                    raise ValueError(
+                        "can not find length={},width={} in item list".format(length, width))
+
+        best_segment = dp_segment[self._W]
         print("generate segment x: {} complete".format(x))
-        
+
         return best_segment
-    
+
     def export_segment_figures(self):
-        for i,segment in enumerate(self._segments):
+        for i, segment in enumerate(self._segments):
             plt.clf()
             plt.cla()
-            fig,ax=plt.subplots()
+            fig, ax = plt.subplots()
             segment.plot(ax)
             plt.plot()
-            figure_path=os.path.join(segment_figures_dir,"{}_{}.png".format(i,segment.x))
+            figure_path = os.path.join(
+                segment_figures_dir, "{}_{}.png".format(i, segment.x))
             plt.savefig(figure_path)
-    
-    def segments_item_amount(self,item_length,item_width):
+
+    def segments_item_amount(self, item_length, item_width):
         """computing the number of the given item in current segments
 
         Args:
@@ -233,14 +249,15 @@ class PatternGenerator:
         Returns:
             item_amount: an integer representing the number of the given item
         """
-        item_amount=sum(
-                [
-                    seg.item_amount(item_length,item_width)+seg.item_amount(item_width,item_length) 
-                    for seg in self._segments
-                ]
-            )
+        item_amount = sum(
+            [
+                seg.item_amount(item_length, item_width) +
+                seg.item_amount(item_width, item_length)
+                for seg in self._segments
+            ]
+        )
         return item_amount
-    
+
     def check_segments_coverage(self):
         """check if the current segments cover all the item type
 
@@ -248,35 +265,33 @@ class PatternGenerator:
             bool: True means the current segments have covered all the item type,
             False means there exists item type which has not been covered
         """
-        for _,row in self._origin_df.iterrows():
-            item_length=row["item_length"]
-            item_width=row["item_width"]
-            item_amount=self.segments_item_amount(item_length,item_width)
-            if item_amount==0:
+        for _, row in self._origin_df.iterrows():
+            item_length = row["item_length"]
+            item_width = row["item_width"]
+            item_amount = self.segments_item_amount(item_length, item_width)
+            if item_amount == 0:
                 # print("item whose length is {} and width is {} is not covered".format(item_length,item_width))
                 return False
         return True
-    
+
     def reduce_require_item_from_segments(self):
         for seg in self._segments:
             for strip in seg.strips:
-                self.reduce_item_require(strip.l,strip.w,strip.e)
-    
-    def export_segments(self,export_path):
-        pass
-     
+                self.reduce_item_require(strip.l, strip.w, strip.e)
+
     def generate_pattern(self):
         pass
-        
-if __name__=="__main__":
-    df=pd.read_csv(dataA_paths[0])
-    pg=PatternGenerator(df,L=2440,W=1220)
+
+
+if __name__ == "__main__":
+    df = pd.read_csv(dataA_paths[0])
+    pg = PatternGenerator(df, L=2440, W=1220)
     print(len(pg._item_require_num))
     print(len(pg._item_shape))
     print(len(pg._q))
     pg.check_segment_coverage()
     pg.export_segment_figures()
     pg.generate_segment_x(2440)
-    
+
     # print(pg.df.sort_values(by=["item_length","item_width"]))
     # print(pg.df.describe())
--- utils.py
+++ utils.py
@@ -2,8 +2,9 @@ from matplotlib import pyplot as plt
 from matplotlib import patches
 import dill
 
+
 class Strip:
-    def __init__(self,x,l,w,e,v) -> None:
+    def __init__(self, x, l, w, e, v) -> None:
         """_summary_
 
         Args:
@@ -13,69 +14,69 @@ class Strip:
             e (float): max number of items in the strip
             v (float): value of the strip
         """
-        self._x=x
-        self._l=l
-        self._w=w
-        self._e=e
-        self._v=v
-        
+        self._x = x
+        self._l = l
+        self._w = w
+        self._e = e
+        self._v = v
+
     def __str__(self) -> str:
         return f"x: {self._x}, l:{self._l}, w:{self._w}, e:{self._e}, v:{self._v}"
-    
-    def plot(self,ax,left,bottom):
+
+    def plot(self, ax, left, bottom):
         for i in range(self.e):
-            rect=patches.Rectangle(
-                (left+i*self._l,bottom),
+            rect = patches.Rectangle(
+                (left+i*self._l, bottom),
                 width=self._l,
                 height=self._w,
                 fill=False
-                )
+            )
             ax.add_patch(rect)
             print
         plt.plot()
-            
+
     @property
     def x(self):
         return self._x
-    
+
     @property
     def l(self):
         return self._l
-    
+
     @property
     def w(self):
         return self._w
-    
+
     @property
     def e(self):
         return self._e
-    
+
     @property
     def v(self):
         return self._v
-    
+
 
 class Segment:
-    def __init__(self,x,strips) -> None:
-        self._x=x
-        self._strips=strips
-        
+    def __init__(self, x, strips) -> None:
+        self._x = x
+        self._strips = strips
+
     def __str__(self) -> str:
-        s=""
+        s = ""
         for strip in self._strips:
-            s+=str(strip)+"\n"
+            s += str(strip)+"\n"
         return s
-    
-    def append(self,strip):
+
+    def append(self, strip):
         self._strips.append(strip)
-        
-    def plot(self,ax,left=0):
-        bottom=0
+
+    def plot(self, ax, left=0):
+        bottom = 0
         for strip in self._strips:
-            strip.plot(ax,left,bottom)
-            bottom+=strip.w
-            
-    def item_amount(self,length,width):
+            strip.plot(ax, left, bottom)
+            bottom += strip.w
+
+    def item_amount(self, length, width):
         """amount of item whose length and width is given
 
         Args:
@@ -85,62 +86,65 @@ class Segment:
         Returns:
             amount: int, number of the item
         """
-        amount=0
+        amount = 0
         for strip in self._strips:
-            if strip.l==length and strip.w==width:
-                amount+=strip.e
+            if strip.l == length and strip.w == width:
+                amount += strip.e
         return amount
-    
+
     @property
     def x(self):
         return self._x
-    
+
     @property
     def strips(self):
         return self._strips
-        
+
     @property
     def v(self):
-        v=0
+        v = 0
         for strip in self._strips:
-            v+=strip.v
+            v += strip.v
         return v
-    
+
     @property
     def empty(self):
         return not self._strips
-    
+
+
 class Pattern:
-    def __init__(self,segments) -> None:
-        self._segments=segments
-    
+    def __init__(self, segments) -> None:
+        self._segments = segments
+
     def __str__(self) -> str:
-        s=""
+        s = ""
         for segment in self._segments:
-            s+=segment+"\n"
+            s += segment+"\n"
         return s
-    
-    def plot(self,ax):
-        left=0
+
+    def plot(self, ax):
+        left = 0
         for segment in self._segments:
-            segment.plot(ax,left)
-            left+=segment.x
-    
+            segment.plot(ax, left)
+            left += segment.x
+
 
 def dill_load(path):
-    with open(path,"rb") as f:
-        obj=dill.load(f)
+    with open(path, "rb") as f:
+        obj = dill.load(f)
         f.close()
     return obj
 
-def dill_save(obj,path):
-    with open(path,"wb") as f:
-        dill.dump(obj,f)
+
+def dill_save(obj, path):
+    with open(path, "wb") as f:
+        dill.dump(obj, f)
         f.close()
 
-if __name__=="__main__":
-    strip=Strip(50,10,20,2,200)
-    seg=Segment(1,[strip,strip,strip])
-    fig,ax=plt.subplots()
-    seg.plot(ax,0)
+
+if __name__ == "__main__":
+    strip = Strip(50, 10, 20, 2, 200)
+    seg = Segment(1, [strip, strip, strip])
+    fig, ax = plt.subplots()
+    seg.plot(ax, 0)
     plt.show()
