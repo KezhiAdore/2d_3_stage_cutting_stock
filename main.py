713ec38602430cabdfa7af3da25f27954f3d466f
gwyxjtu <867718012@qq.com>
2022-10-09 16:22:12 +0800

    :white_check_mark: ready to test main

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def solve_batch(args)->PatternGenerator:
     pg=PatternGenerator(df,L,W,multi_process=False)
     pg.generate_patterns()
     pg.export_patterns()
-    # pg.export_pattern_figure(batch_figure_dir)
+    pg.export_pattern_figure(batch_figure_dir)
     return pg
             
 def solve_A(args):
@@ -54,14 +54,11 @@ def solve_B(args):
     # get data name
     data_prefix=data_path[data_path.rfind("/")+1:].replace(".csv","")
     
-    order_info=presolve_csv(data_path,data_prefix)
-    try:
-        batch_info=batch_generate(order_info,data_prefix,batch_size,use_cache=False)
-        divided_csv(data_path,batch_info,data_prefix+f"_{batch_size}")
-    except:
-        return
+    order_info=presolve_csv(data_path,data_prefix,use_cache=False)
+    batch_info=batch_generate(order_info,data_prefix,batch_count=batch_size,use_cache=False)
+    divided_csv(data_path,batch_info,data_prefix)
     
-    divided_csv_dir=os.path.join(division_dir,data_prefix+f"_{batch_size}")
+    divided_csv_dir=os.path.join(division_dir,data_prefix)
     batch_num_list=os.listdir(divided_csv_dir)
     
     msgs=[]
@@ -74,8 +71,8 @@ def solve_B(args):
             msgs.append((batch_num,material,filepath,batch_figure_dir,L,W))
     
     # multiprocessing computing
-    with ProcessPoolExecutor(60) as pool:
-        results=pool.map(solve_batch,msgs,chunksize=5)
+    with ProcessPoolExecutor(1) as pool:
+        results=pool.map(solve_batch,msgs)
         
     # sort results
     pg_dict={}
@@ -87,6 +84,7 @@ def solve_B(args):
         pg_dict[batch_num][material]=pg
     
     result={}
+    
     # export answer
     plate_id=0
     last_batch_plate_id=0
@@ -108,14 +106,14 @@ def solve_B(args):
         batch_plate_num=plate_id-last_batch_plate_id
         last_batch_plate_id=plate_id
         batch_plate_area=batch_plate_num*L*W
-        # result[batch_num]={
-        #     "batch_plate_num": batch_plate_num,
-        #     "batch_plate_area": batch_plate_area,
-        #     "batch_require_area": batch_require_area,
-        #     "batch_use_ratio": batch_require_area/batch_plate_area,
-        # }
+        result[batch_num]={
+            "batch_plate_num": batch_plate_num,
+            "batch_plate_area": batch_plate_area,
+            "batch_require_area": batch_require_area,
+            "batch_use_ratio": batch_require_area/batch_plate_area,
+        }
             
-    # ans_df.to_csv(ans_path)
+    ans_df.to_csv(ans_path)
     
     total_plate_area=plate_id*L*W
     use_ratio=total_require_area/total_plate_area
@@ -127,7 +125,7 @@ def solve_B(args):
         "total_require_area": total_require_area,
         "total_use_ratio": use_ratio,
     })
-    result_path=result_path[:result_path.rfind(".json")]+"_"+str(batch_size)+".json"
+    result_path=result_path[:result_path.rfind(".json")]+".json"
     json_save(result,result_path)
     return result
 
@@ -160,25 +158,16 @@ if __name__=="__main__":
         # solve problem B
         start_time=datetime.datetime.now()
         msgs=[]
-        for batch_size in range(60,30,-1):
-            msgs.extend([(
+        best_batch_size=[36,31,36,36,36]
+        msgs.extend([(
             dataB_paths[i],
             os.path.join(P2_ans_dir,f"B{i+1}.csv"),
             os.path.join(pattern_figures_dir,f"B{i+1}"),
             os.path.join(P2_ans_dir,f"B{i+1}.json"),
-            L,W,batch_size) for i in range(5)])
+            L,W,best_batch_size[i]) for i in range(5)])
         
         with ProcessPoolExecutor(5) as pool:
-            results = pool.map(solve_B,msgs,chunksize=30)
-        
-        test_result={}
-        for i,r in enumerate(results):
-            batch_size=msgs[i][-1]
-            if batch_size not in test_result.keys():
-                test_result[batch_size]=[]
-            test_result[batch_size].append(r)
-        
-        json_save(test_result,"test_B.json")
+            results = pool.map(solve_B,msgs)
         
         end_time=datetime.datetime.now()
         time_cost=end_time-start_time
