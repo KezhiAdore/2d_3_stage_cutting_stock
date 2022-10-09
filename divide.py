e85615f06c4e868608d9c271783d554c4e9f231f
KezhiAdore <1538001851@qq.com>
2022-10-09 18:49:10 +0800

    :bug: fix dividing bugs

--- divide.py
+++ divide.py
@@ -22,6 +22,10 @@ def divided_csv(path,batch_info,data_prefix):
     #     print("-----------------------")
     # print(dataframe_batch_list[3])
     # print('finish')
+    
+    # clear division cache
+    data_division_dir=os.path.join(division_dir,data_prefix)
+    os.system(f"rm -rf {data_division_dir}")
         
     for i in range(len(dataframe_batch_list)):
         # 按照item_material拆分dataframe
